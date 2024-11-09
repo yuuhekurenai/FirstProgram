17f4e4d849ddfa283205d12352d2de831958386b
Yasuzo <yuuhekurenai@gmail.com>
2024-11-09 17:26:04 -0300

    dasafio4 entregue

---
+++ Exercicios/desafio4.rb
@@ -0,0 +1,20 @@
+#Desafios Ruby na DIO têm funções "gets" e "puts" acessíveis globalmente:
+# "gets" : lê UMA linha com dado(s) de entrada (inputs) do usuário;
+# "puts": imprime um texto de saída (output), pulando linha.
+# ".to_i": Converte para int
+# "require'bigdecimal'": É a importação de uma biblioteca BigDecimal
+#que fornece suporte para operações aritméticas muito mais precisas
+#utilizando números decimais. Para utiliza-la usamos BigDecimal(AVariavel)
+
+require 'bigdecimal'
+
+line = gets.split(" ")
+a = BigDecimal(line[0])
+b = BigDecimal(line[1])
+
+#TODO:  Crie as condições necessárias para calcular o número
+
+#médio de coxinhas
+media = a / b
+
+puts format('%.2f', media)
